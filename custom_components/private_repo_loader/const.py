3819a546d7b628ab2cf9239147f9f5803444ba40
BitBasherr <betalik@outlook.com>
2025-07-06 16:13:07 -0500

    update to retain backwards compatibility

--- custom_components/private_repo_loader/const.py
+++ custom_components/private_repo_loader/const.py
@@ -1,8 +1,11 @@
 DOMAIN = "private_repo_loader"
 
-CONF_REPO = "repository"
+# names used by the various modules ------------------------------
+CONF_REPO  = "repository"
+CONF_TOKEN = "token"          # preferred new name
+CONF_PAT   = CONF_TOKEN       # <- <-- alias keeps old code working
 
+# storage keys ----------------------------------------------------
 STORAGE_VERSION = 1
-STORAGE_KEY = f"{DOMAIN}.storage"
-
-DATA_REPOS = "repos"
+STORAGE_KEY     = f"{DOMAIN}.storage"
+DATA_REPOS      = "repos"
