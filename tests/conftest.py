00ba6b258771ee3df11803bf569677fdc4954d69
BitBasherr <BitBasherr@users.noreply.github.com>
2025-07-07 09:07:30 +0000

    chore: format code with Black

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,5 @@ import sys
 # Make custom_components importable for all tests
 sys.path.insert(
     0,
-    os.path.abspath(
-        os.path.join(os.path.dirname(__file__), "..")
-    ),
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "..")),
 )
