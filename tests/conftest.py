cc44d08dc1dcc42927b6fa0d22a24b91f3b7166a
BitBasherr <betalik@outlook.com>
2025-07-07 04:06:33 -0500

    Create conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+# Make custom_components importable for all tests
+sys.path.insert(
+    0,
+    os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "..")
+    ),
+)
