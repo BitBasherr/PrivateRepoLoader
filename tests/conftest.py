c311d5dda9cb06c49e5ab4777536e181edc08df4
BitBasherr <BitBasherr@users.noreply.github.com>
2025-07-07 09:13:02 +0000

    chore: format code with Black

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,11 +4,10 @@ import sys
 # Make custom_components importable for all tests
 sys.path.insert(
     0,
-    os.path.abspath(
-        os.path.join(os.path.dirname(__file__), "..")
-    ),
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "..")),
 )
 
 # Override _auth_url so file:// URLs work in tests
 import custom_components.private_repo_loader.loader as loader_module  # noqa: E402
+
 loader_module._auth_url = lambda url, token: url
